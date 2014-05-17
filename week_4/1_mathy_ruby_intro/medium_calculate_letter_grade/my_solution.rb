945e0ca5db28a96b99f47638653964fb07eb577d
Rootul <rootulp@gmail.com>
2014-05-17 03:56:37 -0400

    completed calculate letter grade

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -4,22 +4,49 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
 # What is the input?
+# An array
 # What is the output? (i.e. What should the code return?)
+# Return a letter grade
 # What are the steps needed to solve the problem?
-
+# Define method get_grade that takes one argument (array)
+# Find the average of the array and depending on that, return a letter grade that corresponds to the value
 
 # 2. Initial Solution
-
-
-
+# def get_grade(x)
+#     average = x.inject(0.0) { |sum, el| sum + el } / x.size
+#     if average >= 90
+#         return "A"
+#     elsif average >= 80
+#         return "B"
+#     elsif average >= 70
+#         return "C"
+#     elsif average >= 60
+#         return "D"
+#     else 
+#         return "F"
+#     end      
+# end
 
 # 3. Refactored Solution
-
-
+def get_grade(x)
+    average = x.inject(0.0) { |sum, el| sum + el } / x.size
+    if average >= 90
+        return "A"
+    elsif average >= 80
+        return "B"
+    elsif average >= 70
+        return "C"
+    elsif average >= 60
+        return "D"
+    else 
+        return "F"
+    end      
+end
 
 # 4. Reflection 
+# I enjoyed this challenge. I think it was easier than the easy challenge though. Maybe someone should swap the two. 
